ffb8ce950056adc4e770219b558fd049c15a18fa
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2024-10-14 00:13:10 +0200

    feat: better logging, argv validation (for data folder)

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,79 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/lmittmann/tint"
+	"github.com/pborman/getopt/v2"
+)
+
+type Opts struct {
+	dataDir string
+}
+
+func doFolderExists(path string) (bool, error) {
+	stat, err := os.Stat(path)
+
+	if err == nil {
+		return stat.IsDir(), nil
+	}
+
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+
+	return false, err
+}
+
+func parseOpts() Opts {
+	opts := Opts{}
+
+	// definition
+	help := getopt.BoolLong("help", 'h', "Shows the help menu")
+	dataDir := getopt.StringLong("data-dir", 'd', "", "Path of the data folder (containing html, json, ...)")
+
+	// parsing
+	getopt.Parse()
+
+	if *help {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
+	absDataDir, err := filepath.Abs(*dataDir)
+	if err != nil {
+		tint.Err(err)
+		os.Exit(1)
+	}
+	opts.dataDir = absDataDir
+
+	dataDirExists, err := doFolderExists(opts.dataDir)
+
+	if !dataDirExists {
+		slog.Error("You must set the --data-dir flag to an existing directory.")
+		os.Exit(2)
+	}
+	if err != nil {
+		tint.Err(err)
+		os.Exit(1)
+	}
+
+	return opts
+}
+
 func main() {
-	print("Hello world")
+	slog.SetDefault(
+		slog.New(
+			tint.NewHandler(os.Stderr, &tint.Options{
+				Level:      slog.LevelDebug,
+				TimeFormat: time.Kitchen,
+			}),
+		))
+
+	opts := parseOpts()
+
+	slog.Info("argv validation", "data_dir", opts.dataDir)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/PoliNetworkOrg/rankings-backend-go
 
 go 1.23.2
+
+require github.com/lmittmann/tint v1.0.5
+
+require github.com/pborman/getopt/v2 v2.1.0
