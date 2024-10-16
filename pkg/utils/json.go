fcaa75e09c39c010d766cc9904aa7eddea1803bc
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2024-10-16 22:05:51 +0200

    chore: warn when using TestJsonEquals
    reason in warn message

--- pkg/utils/json.go
+++ pkg/utils/json.go
@@ -2,18 +2,20 @@ package utils
 
 import (
 	"encoding/json"
+	"log/slog"
 	"reflect"
 )
 
 func TestJsonEquals(first, second []byte) (bool, error) {
+	slog.Warn("TestJsonEquals => this method is not reliable if data is not equally sorted, because it performs a DeepEqual which compares item one-by-one. Use it only if you are sure that data is sorted equally.")
 	var firstDec, secondDec interface{}
 	err := json.Unmarshal(first, &firstDec)
-	if err != nil  {
+	if err != nil {
 		return false, err
 	}
 
 	err = json.Unmarshal(second, &secondDec)
-	if err != nil  {
+	if err != nil {
 		return false, err
 	}
 
