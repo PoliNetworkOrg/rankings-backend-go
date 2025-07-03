7c029b1c9ccadc13a47ea5fdee61cf2aeb1d5d35
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-07-03 22:37:27 +0200

    fix: 429 error in HTTP loadDoc fn, missing user-agent in req headers

--- pkg/scraper/scraper.go
+++ pkg/scraper/scraper.go
@@ -154,14 +154,21 @@ func ScrapeManifesti(alreadyScraped []Manifesto) []Manifesto {
 }
 
 func loadDoc(url string) (*goquery.Document, *http.Response, error) {
-	res, err := http.Get(url)
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+	res, err := client.Do(req)
 	if err != nil {
 		return nil, nil, err
 	}
 
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		return nil, nil, fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
+		return nil, nil, fmt.Errorf("HTTP code is not 200. Status: %s", res.Status)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
