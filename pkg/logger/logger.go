9a97d940b1c4c40dee7f48f27c7b8ec6949f7124
Lorenzo <lorenzo.corallo@outlook.it>
2025-08-08 16:57:35 +0200

    feat: set logger level via LOG_LEVEL env variable

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -3,6 +3,7 @@ package logger
 import (
 	"log/slog"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/lmittmann/tint"
@@ -15,8 +16,37 @@ func GetDefaultLogger() *slog.Logger {
 	return slog.New(
 		tint.NewHandler(w, &tint.Options{
 			AddSource:  true,
-			Level:      slog.LevelDebug,
+			Level:      getEnvLogLevel(slog.LevelInfo),
 			TimeFormat: time.Kitchen,
 			NoColor:    !isatty.IsTerminal(w.Fd()),
 		}))
+}
+
+func getEnvLogLevel(defaultLevel slog.Level) slog.Level {
+	// Read log level from environment variable
+	level := defaultLevel
+	if lvlStr := os.Getenv("LOG_LEVEL"); lvlStr != "" {
+		if parsed, err := parseLogLevel(lvlStr); err == nil {
+			level = parsed
+		} else {
+			slog.Warn("[slog] Invalid LOG_LEVEL env value, fallback to default. Supported levels (case-insensitive): debug, info, warn, error", "default", lvlStr)
+		}
+	}
+
+	return level
+}
+
+func parseLogLevel(s string) (slog.Level, error) {
+	switch strings.ToLower(s) {
+	case "debug":
+		return slog.LevelDebug, nil
+	case "info":
+		return slog.LevelInfo, nil
+	case "warn", "warning":
+		return slog.LevelWarn, nil
+	case "error":
+		return slog.LevelError, nil
+	default:
+		return slog.LevelInfo, os.ErrInvalid
+	}
 }
