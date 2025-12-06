78b6f924a619127872cd351f4b7771aeac55fbe8
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-12-06 03:48:37 +0100

    fix: polimi has troubles with www

--- pkg/constants/web.go
+++ pkg/constants/web.go
@@ -5,13 +5,13 @@ const (
 	WebPolimiAvvisiFuturiStudentiUrl       = "https://www.polimi.it/futuri-studenti/avvisi"
 
 	// useful to obtain school's manifest link
-	WebPolimiDesignUrl    = "https://polimi.it/formazione/corsi-di-laurea/dettaglio-corso/design-degli-interni"
-	WebPolimiIngCivUrl    = "https://polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-per-lambiente-e-il-territorio"
-	WebPolimiIngInfIndUrl = "https://polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-informatica"
-	WebPolimiArchUrbUrl   = "https://polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-edile-architettura"
+	WebPolimiDesignUrl    = "https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/design-degli-interni"
+	WebPolimiIngCivUrl    = "https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-per-lambiente-e-il-territorio"
+	WebPolimiIngInfIndUrl = "https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-informatica"
+	WebPolimiArchUrbUrl   = "https://www.polimi.it/formazione/corsi-di-laurea/dettaglio-corso/ingegneria-edile-architettura"
 
-	WebGithubStableDataUrl = "https://github.com/PoliNetworkOrg/RankingsDati/tree/stable/data"
-	WebGithubMainDataUrl = "https://github.com/PoliNetworkOrg/RankingsDati/tree/main/data"
-	WebGithubMainRawDataUrl = "https://raw.githubusercontent.com/PoliNetworkOrg/RankingsDati/refs/heads/main/data"
+	WebGithubStableDataUrl    = "https://github.com/PoliNetworkOrg/RankingsDati/tree/stable/data"
+	WebGithubMainDataUrl      = "https://github.com/PoliNetworkOrg/RankingsDati/tree/main/data"
+	WebGithubMainRawDataUrl   = "https://raw.githubusercontent.com/PoliNetworkOrg/RankingsDati/refs/heads/main/data"
 	WebGithubStableRawDataUrl = "https://raw.githubusercontent.com/PoliNetworkOrg/RankingsDati/refs/heads/stable/data"
 )
